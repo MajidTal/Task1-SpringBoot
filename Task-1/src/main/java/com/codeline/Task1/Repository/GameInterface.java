840a32efa172d4cc689f6065d0b3586ec36e319e
MajidTal <MajidAlhadidi@outlook.com>
2023-05-21 15:46:39 +0400

    Created a interface class with the name of GameInterface

--- Task-1/src/main/java/com/codeline/Task1/Repository/GameInterface.java
+++ Task-1/src/main/java/com/codeline/Task1/Repository/GameInterface.java
@@ -1,4 +1,4 @@
 package com.codeline.Task1.Repository;
 
-public class LeaderboardRepository {
+public interface GameInterface {
 }
--- Task-1/src/main/java/com/codeline/Task1/Repository/RegistrationRepository.java
+++
@@ -1,4 +0,0 @@
-package com.codeline.Task1.Repository;
-
-public class RegistrationRepository {
-}
