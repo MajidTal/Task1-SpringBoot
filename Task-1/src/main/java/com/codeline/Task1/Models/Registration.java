c11da57c53ab5c28d4722b4e7ad5387bdbfc0902
MajidTal <MajidAlhadidi@outlook.com>
2023-05-21 16:37:07 +0400

    Created a class with the name of BaseEntity in a models package

--- Task-1/src/main/java/com/codeline/Task1/Models/Registration.java
+++ Task-1/src/main/java/com/codeline/Task1/Models/Registration.java
@@ -1,4 +1,15 @@
 package com.codeline.Task1.Models;
 
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Setter
+@Getter
+@Data
+@Entity
 public class Registration {
 }
