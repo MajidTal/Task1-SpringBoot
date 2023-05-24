312eefec074d0afffd270e0594e6c8948130af83
MajidTal <MajidAlhadidi@outlook.com>
2023-05-24 15:10:16 +0400

    I add create a game method and find game method

--- src/main/java/com/codeline/Task1/Service/GameService.java
+++ src/main/java/com/codeline/Task1/Service/GameService.java
@@ -1,14 +1,20 @@
 package com.codeline.Task1.Service;
 
+import com.codeline.Task1.Models.Game;
 import com.codeline.Task1.Repository.GameInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GameService {
     @Autowired
     GameInterface gameInterface;
 
+    public void saveGame(Game game) {gameInterface.save(game);
+    }
+    public List<Game> getGame() { return gameInterface.findAll();}
 
 
 
