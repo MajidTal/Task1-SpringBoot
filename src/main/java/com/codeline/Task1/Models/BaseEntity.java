37909565282d18dc4cf3259ec74c44e0c9f11d46
MajidTal <MajidAlhadidi@outlook.com>
2023-05-24 11:22:15 +0400

    add a variable createdDate in a BaseEntity

--- src/main/java/com/codeline/Task1/Models/BaseEntity.java
+++ src/main/java/com/codeline/Task1/Models/BaseEntity.java
@@ -1,4 +1,17 @@
 package com.codeline.Task1.Models;
 
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@Getter
+@Setter
+@Data
 public class BaseEntity {
+
+    Date createdDate;
 }
--- src/main/java/com/codeline/Task1/Models/Management.java
+++ src/main/java/com/codeline/Task1/Models/Management.java
@@ -6,10 +6,16 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Setter
 @Getter
 @Data
 @Entity
 public class Management {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    Long id;
 }
